ba2f0c53b9e1126f2b6d6edd2d48f734f0919022
riskanurulfajriani <riskanurulfajriani@gmail.com>
2025-11-30 13:09:42 +0700

    Add files via upload

---
+++ tugas_2/diskon.php
@@ -0,0 +1,30 @@
+<?php
+
+// Fungsi untuk menghitung diskon
+function hitungDiskon($totalBelanja)
+{
+    $diskon = 0;
+
+    if ($totalBelanja >= 100000) {
+        $diskon = 0.10 * $totalBelanja;  // Diskon 10%
+    } elseif ($totalBelanja >= 50000) {
+        $diskon = 0.05 * $totalBelanja;  // Diskon 5%
+    }
+
+    return $diskon;
+}
+
+// Contoh total belanja
+$totalBelanja = 1200000;
+
+// Hitung diskon
+$diskon = hitungDiskon($totalBelanja);
+
+// Hitung total bayar
+$totalBayar = $totalBelanja - $diskon;
+
+// Tampilkan output
+echo "=== Perhitungan Diskon ===<br>";
+echo "Total Belanja : Rp. " . number_format($totalBelanja, 0, ',', '.') . "<br>";
+echo "Diskon        : Rp. " . number_format($diskon, 0, ',', '.') . "<br>";
+echo "Total Bayar   : Rp. " . number_format($totalBayar, 0, ',', '.') . "<br>";
